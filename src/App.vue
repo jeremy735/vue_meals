9447ea1550bffde983543d43064924aea5853246
jeremy <xilms735@hotmail.com>
2023-02-24 18:23:50 +0800

    my New Project vue_meals

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-gray-100 h-full">
+      <header class="bg-white shadow h-16 flex justify-between items-center">
+        <router-link :to="{name:'home'}">
+          Home
+        </router-link>
+        <div>
+          <router-link to="/">
+            Search Meals
+          </router-link>
+          <router-link to="/">
+            Meals By Letter
+          </router-link>
+          <router-link to="/">
+            Meals By Ingredients
+          </router-link>
+        </div>
+
+      </header>
+      <main>
+        <router-view />
+      </main>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
