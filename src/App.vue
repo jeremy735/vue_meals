48204ad5eb40c7c2407d3b65d403e5eec6e12d46
jeremy <xilms735@hotmail.com>
2023-02-25 21:49:08 +0800

    add defaultlayout , guestlayout and navbar  router page & meals by ingredient ,by letter by name page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
-  <div class="bg-gray-100 h-full">
-      <header class="bg-white shadow h-16 flex justify-between items-center">
-        <router-link :to="{name:'home'}">
-          Home
-        </router-link>
-        <div>
-          <router-link to="/">
-            Search Meals
-          </router-link>
-          <router-link to="/">
-            Meals By Letter
-          </router-link>
-          <router-link to="/">
-            Meals By Ingredients
-          </router-link>
-        </div>
-
-      </header>
-      <main>
-        <router-view />
-      </main>
-  </div>
+  <router-view />
 </template>
 
 <script setup>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,47 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import GuestLayout from '../components/GuestLayout.vue';
 import Home from '../views/Home.vue'
-import MealList from '../views/MealList.vue'
-
+import MealsByIngredient from '../views/MealsByIngredient.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByName from '../views/MealsByName.vue'
 
 const routes =[
     {
-        path: "/",
-        name: "home",
-        component: Home,
-    },
+        path: '/',
+        component: DefaultLayout,
+        children:[
+            {
+                path: "/",
+                name: "home",
+                component: Home,
+            },
+            {
+                path: "/By-name/:name?",
+                name: "byName",
+                component: MealsByName,
+            },
+            {
+                path: "/By-lettet/:letter?",
+                name: "byLetter",
+                component: MealsByLetter,
+            },
+            {
+                path: "/By-ingredient/:ingredient?",
+                name: "byIngredient",
+                component: MealsByIngredient,
+            },
+        ]
+    },   
     {
-        path: "/letter/:letter",
-        name: "byLetter",
-        component: MealList,
-    },
+        path: '/guest',
+        component: GuestLayout,
+    } 
 ];
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes
+    routes,
 })
 
 export default router;
